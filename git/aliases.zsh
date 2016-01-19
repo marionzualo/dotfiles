c098429cb9eac7773b16a79588359454d4bca6c8
Mário Nzualo <mario.nzualo@seedrs.com>
2016-01-19 13:02:33 +0000

    Add aliases to git
    Use the built in git aliases instead of system aliases for frequent
    commands. Using this provides completion when needed.
    The redundant cases have been removed.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,21 +7,10 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
 alias gpb='git checkout $(git branch | cut -c 3- | pick)'
 alias gcb='git copy-branch-name'
-alias gb='git branch'
 alias gdb='git branch -D $(git branch | cut -c 3- | pick)'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gri='git rebase -i'
 alias gpri='git rebase -i $(git branch | cut -c 3- | pick)'
-alias gr='git rebase'
 alias ggui='/usr/local/opt/git/libexec/git-core/git-gui &'
-alias gpf='git push -f origin'
 alias gm='git merge'
