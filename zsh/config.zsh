83a6a5f754b343f240f31ef7f8636b2c93dce264
Mário Nzualo <mario.nzualo@gmail.com>
2017-01-19 17:44:59 +0100

    Replace $ZSH with $DOTFILES_DIR
    $ZSH is now a different thing due to the changes in .zshrc

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,9 +1,9 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
+fpath=($DOTFILES_DIR/functions $fpath)
 
-autoload -U $ZSH/functions/*(:t)
+autoload -U $DOTFILES_DIR/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
