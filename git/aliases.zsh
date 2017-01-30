c68833192385f86cdffae579f46a334cf5785d90
Mário Nzualo <mario.nzualo@gmail.com>
2017-01-30 16:13:30 +0100

    Replace method for aliasing git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,4 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
+eval "$(hub alias -s)"
 
 # The rest of my fun git aliases
 alias ggui='/usr/local/opt/git/libexec/git-core/git-gui &'
