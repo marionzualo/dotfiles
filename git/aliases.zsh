a51c44d9fbf4c3d0b28864053eb1c3543ad8cf08
Mário Nzualo <mario.nzualo@gmail.com>
2017-01-30 16:11:52 +0100

    Remove unnecessary aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,11 +7,5 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gd='git diff'
-alias gpb='git checkout $(git branch | cut -c 3- | pick)'
-alias gcb='git copy-branch-name'
-alias gdb='git branch -D $(git branch | cut -c 3- | pick)'
-alias gpri='git rebase -i $(git branch | cut -c 3- | pick)'
 alias ggui='/usr/local/opt/git/libexec/git-core/git-gui &'
-alias gm='git merge'
 alias editgit='vim ~/.gitconfig'
