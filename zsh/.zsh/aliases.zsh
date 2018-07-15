6dd073a3d910fc08c9a71a5da369de2070a077b2
Mário  Nzualo <mario.nzualo@gmail.com>
2018-07-15 14:39:14 +0100

    Add zsh config

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,4 @@
+alias vim=nvim
+alias reload!='source ~/.zshrc'
+alias rs='rails s'
+alias rc='rails c'
---
+++ zsh/.zsh/environment.zsh
@@ -0,0 +1,17 @@
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+#preferred editor for local and remote sessions
+export EDITOR=vim
+export VISUAL=vim
+
+#language
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LESSCHARSET=utf-8
